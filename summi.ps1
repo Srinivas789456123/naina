ca0a620a114b455acd5d1e5fbc682621b7ef85d5
Srinivas789456123 <168059599+Srinivas789456123@users.noreply.github.com>
2024-04-30 11:01:37 +0530

    Add files via upload

---
+++ summi.ps1
@@ -0,0 +1,13 @@
+﻿md parent
+md parent\c1
+md parent\c2
+md parent\c3
+md parent\c4
+echo "welcome To C1T1" > parent\c1\C1T1.txt
+echo "welcome To C2T2" > parent\c2\C2T2.txt
+echo "welcome To C3T3" > parent\C3\C3T3.txt
+echo "welcome To C4T4" > parent\c4\C4T4.txt
+type parent\C1\C1T1.txt
+type parent\C2\C2T2.txt
+type parent\C3\C3T3.txt
+type parent\C4\C4T4.txt
